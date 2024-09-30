e9352d0a1c0384ea8c094433c5520f4e8ab98392
Audi Atmaja <audiatmaja@gmail.com>
2024-09-30 10:37:44 +0700

    ubah kualitas logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,112 +1,110 @@
 <html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="/assets/" data-template="vertical-menu-template-free">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>DPPKUM Kota Magelang</title>
+    <title>DPPKUM Kota Magelang</title>
 
-  <meta name="description" content="" />
+    <meta name="description" content="" />
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
-  <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="/assets/vendor/fonts/boxicons.css" />
-  <link rel="icon" type="image/svg" href="/assets/img/logo-kota-magelang.svg" />
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="/assets/vendor/fonts/boxicons.css" />
+    <link rel="icon" type="image/svg" href="/assets/img/logo-kota-magelang.svg" />
 
-  <!-- Core CSS -->
-  <link rel="stylesheet" href="/assets/vendor/css/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="/assets/css/demo.css" />
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="/assets/css/demo.css" />
 
-  <!-- Vendors CSS -->
-  <link rel="stylesheet" href="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-  <!-- Page CSS -->
-  <!-- Page -->
-  <link rel="stylesheet" href="/assets/vendor/css/pages/page-auth.css" />
-  <!-- Helpers -->
-  <script src="/assets/vendor/js/helpers.js"></script>
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="/assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="/assets/vendor/js/helpers.js"></script>
 
-  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="/assets/js/config.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="/assets/js/config.js"></script>
 </head>
 
 <body>
-  <!-- Content -->
-  @include('sweetalert::alert')
-  <div class="container-xxl">
-    <div class="authentication-wrapper authentication-basic container-p-y">
-      <div class="authentication-inner">
-        <!-- Register -->
-        <div class="card">
-          <div class="card-body">
-            <div style="display: flex; flex-direction: column; align-items: center; text-align: center;">
-              <img src="{{ asset('assets/img/logo-dppkum.png') }}" alt="logo-dppkum" style="width: 230px; margin-bottom: 16px;">
-              <!-- <h5 class="mb-3 fw-bold mt-2" style="text-align:center; background: #19497D; background-clip: text; color: transparent;">
-                Unit Pasar <br> Kota Magelang</h5> -->
-            </div>
-            <x-auth-session-status class="mb-4" :status="session('status')" />
-            <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
-              @csrf
-              <div class="mb-3">
-                <x-input-label for="email" class="form-label">
-                    <strong>Username</strong>
-                </x-input-label>
-                <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger fs-6" />
-            </div>
-            
+    <!-- Content -->
+    @include('sweetalert::alert')
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <div style="display: flex; flex-direction: column; align-items: center; text-align: center;">
+                            <img src="{{ asset('assets/img/logo-dppkum.svg') }}" alt="logo-dppkum" style="width: 250px; margin: 10px; margin-left: 20px; margin-bottom: 20px;">
+                        </div>
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+                        <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <x-input-label for="email" class="form-label">
+                                    <strong>Username</strong>
+                                </x-input-label>
+                                <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger fs-6" />
+                            </div>
 
-            <div class="mb-3 form-password-toggle">
-              <div class="d-flex justify-content-between">
-                  <x-input-label for="password" class="form-label">
-                      <strong>Password</strong>
-                  </x-input-label>
-              </div>
-              <div class="input-group input-group-merge">
-                  <x-text-input id="password" class="form-control" type="password" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" required autocomplete="current-password" />
-                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-              </div>
-              <x-input-error :messages="$errors->get('password')" class="mt-2" />
-          </div>
-          
-              <div class="mb-3">
-                <button class="btn d-grid w-100 mb-2" type="submit" style="background-color: #19497D; color: #ffffff;">
-                 <strong> Log in </strong> </button>
-              </div>
-            </form>
-            </p>
-          </div>
+
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <x-input-label for="password" class="form-label">
+                                        <strong>Password</strong>
+                                    </x-input-label>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <x-text-input id="password" class="form-control" type="password" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" required autocomplete="current-password" />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+
+                            <div class="mb-3">
+                                <button class="btn d-grid w-100 mb-2" type="submit" style="background-color: #19497D; color: #ffffff;">
+                                    <strong> Log in </strong> </button>
+                            </div>
+                        </form>
+                        </p>
+                    </div>
+                </div>
+                <!-- /Register -->
+            </div>
         </div>
-        <!-- /Register -->
-      </div>
     </div>
-  </div>
-  <!-- / Content -->
-  <!-- Core JS -->
-  <!-- build:js assets/vendor/js/core.js -->
-  <script src="/assets/vendor/libs/jquery/jquery.js"></script>
-  <script src="/assets/vendor/libs/popper/popper.js"></script>
-  <script src="/assets/vendor/js/bootstrap.js"></script>
-  <script src="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <!-- / Content -->
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="/assets/vendor/libs/popper/popper.js"></script>
+    <script src="/assets/vendor/js/bootstrap.js"></script>
+    <script src="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-  <script src="/assets/vendor/js/menu.js"></script>
-  <!-- endbuild -->
+    <script src="/assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
 
-  <!-- Vendors JS -->
+    <!-- Vendors JS -->
 
-  <!-- Main JS -->
-  <script src="/assets/js/main.js"></script>
+    <!-- Main JS -->
+    <script src="/assets/js/main.js"></script>
 
-  <!-- Page JS -->
+    <!-- Page JS -->
 
-  <!-- Place this tag in your head or just before your close body tag. -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
