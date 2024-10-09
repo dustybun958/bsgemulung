406d4f04c65695aab29bcd7ee488ec90e459091d
Dioraka <rakadio666@gmail.com>
2024-10-09 14:05:28 +0700

    imp pedagang

--- app/Imports/PedagangImport.php
+++ app/Imports/PedagangImport.php
@@ -14,11 +14,14 @@ class PedagangImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
+        // dd($row['id_lapak']);
+        // dd($row['VA']);
+        // dd($row);
         try {
             // Validasi id_lapak
-            $lapak = Lapak::where('id_lapak', $row['id_lapak'])->first();
+            $lapaks = Lapak::where('id_lapak', $row['id_lapak'])->first();
 
-            if (!$lapak) {
+            if (!$lapaks) {
                 Session::flash('error', 'Lapak dengan id_lapak ' . $row['id_lapak'] . ' tidak ditemukan.');
                 return null;
             }
@@ -38,16 +41,20 @@ class PedagangImport implements ToModel, WithHeadingRow
                 return null;
             }
 
+            // Konversi tanggal dari format Excel ke Y-m-d
+            $checkIn = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['check_in'])->format('Y-m-d');
+            $checkOut = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['check_out'])->format('Y-m-d');
+
             return new Pedagang([
                 'id_pedagang' => $row['id_pedagang'],
-                'id_lapak' => $lapak->id_lapak,
+                'id_lapak' => $lapaks->id_lapak,
                 'nik' => $dataDiri->nik,
                 'izin' => $row['izin'],
                 'jenis_dagang' => $row['jenis_dagang'],
-                'check_in' => $row['check_in'],
-                'check_out' => $row['check_out'],
+                'check_in' => $checkIn,
+                'check_out' => $checkOut,
                 'status' => $row['status'],
-                'VA' => $row['VA'],
+                'VA' => $row['va'],
                 'id_penarik_retribusi' => $penarikRetribusi->id_penarik_retribusi,
             ]);
         } catch (\Exception $e) {
